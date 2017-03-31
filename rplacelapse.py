b74c6ff21298cf4619cfae3fa9a1e5b2076ede4a
John Gliksberg <john.gliksberg@atos.net>
2017-03-31 22:51:37 +0200

    Script to transform r/place bitmaps into usable pngs

---
+++ rplacelapse.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+
+colors = [
+    (255, 255, 255),
+    (228, 228, 228),
+    (136, 136, 136),
+    (34, 34, 34),
+    (255, 167, 209),
+    (229, 0, 0),
+    (229, 149, 0),
+    (160, 106, 66),
+    (229, 217, 0),
+    (148, 224, 68),
+    (2, 190, 1),
+    (0, 211, 221),
+    (0, 131, 199),
+    (0, 0, 234),
+    (207, 110, 228),
+    (130, 0, 128)
+]
+
+frame = 1
+while True:
+    filename = "board-bitmap.{}".format(frame)
+    if not os.path.isfile(filename):
+        break
+    print("frame: {}".format(frame))
+    img = Image.new('RGB', (1000, 1000))
+    pixels = img.load()
+    with open(filename, "rb") as f:
+        for y in range(1000):
+            for x in range(500):
+                datum = ord(f.read(1))
+                color1 = datum >> 4
+                color2 = datum - (color1 << 4)
+                color1 = colors[color1]
+                color2 = colors[color2]
+                pixels[x*2,     y] = color1
+                pixels[x*2 + 1, y] = color2
+    img.save(filename + ".png")
+    frame += 1
